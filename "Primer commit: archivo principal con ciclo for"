0064f1b5533a0f9c8163e6f7a9fa0c02a3d4448b
danielaalexandraacosta <danielaalexandraacosta@gmail.com>
2026-03-09 01:08:57 -0500

    "Primer commit: archivo principal con ciclo for"

---
+++ "Primer commit: archivo principal con ciclo for"
@@ -0,0 +1,2 @@
+const numero = parseInt(prompt("Ingrese un numero:"));
+for (let i = 1; i <= 10; i++) {console.log(numero + " x " + i + " = " + (numero * i))}
--- main.js
+++
@@ -1,2 +0,0 @@
-const numero = parseInt(prompt("Ingrese un numero:"));
-for (let i = 1; i <= 10; i++) {console.log(numero + " x " + i + " = " + (numero * i))}
